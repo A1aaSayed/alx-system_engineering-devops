017808104019e509fa417d3036f5f8a8a6807714
A1aaSayed <alaa02.sayed02@gmail.com>
2024-02-01 21:18:30 +0200

    up

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-from = ARGV[0].scan(/from:(\+?\w+)/).join
-to = ARGV[0].scan(/to:(\+?\d{11})/).join
-flags = ARGV[0].scan(/flags:(-?\d:-?\d:-?\d:-?\d:-?\d)/).join
-puts "#{from},#{to},#{flags}"
+puts ARGV[0].scan(/(?<=from:|to:|flags:)[+-]?\S+(?=\])/).join(",")
