e7bc026b2df6648ca000038a398b0d26d84f9bf8
A1aaSayed <alaa02.sayed02@gmail.com>
2024-08-27 04:21:50 -0700

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,24 +3,21 @@
 
 import requests
 
-def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {
-            'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/A1aaSayed)'
-    }
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post.get('data', {}).get('title'))
-            else:
-                print(None)
-        else:
-            print(None)
-    except requests.RequestException:
-        print(None)
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed
+        for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    headers = {'User-Agent': 'Unfortunately I had to use a custom one'}
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 404:
+        print("None")
+        return
+
+    results = response.json().get('data', {})
+    [print(title.get('data', {}).get('title', ''))
+        for title in results.get('children', [])]
