abc11fa8157de3fe3e8fa8b2bc43891d23ddbf20
A1aaSayed <alaa02.sayed02@gmail.com>
2024-08-27 04:16:29 -0700

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+import requests
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {
+            'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/A1aaSayed)'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+            if posts:
+                for post in posts:
+                    print(post.get('data', {}).get('title'))
+            else:
+                print(None)
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
